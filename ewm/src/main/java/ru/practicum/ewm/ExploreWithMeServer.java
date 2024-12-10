60b10d9feb596d07286855b05eb2cb467997dc7b
ernokanst <ernokanst@gmail.com>
2024-12-11 00:32:11 +0300

    Тест

--- ewm/src/main/java/ru/practicum/ewm/ExploreWithMeServer.java
+++ ewm/src/main/java/ru/practicum/ewm/ExploreWithMeServer.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -10,7 +11,6 @@ public class ExploreWithMeServer {
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     public static void main(String[] args) {
-        System.out.println("Main server is on");
-        SpringApplication.run(ExploreWithMeServer.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ExploreWithMeServer.class, args);
     }
 }
