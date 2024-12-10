37f3bbb6c28a2befd7705149ca1c4068e04154d4
ernokanst <ernokanst@gmail.com>
2024-12-11 02:39:17 +0300

    Исправление сервиса статистики

--- stats/stats-service/src/main/java/ru/practicum/stats/exceptions/ExceptionsHandler.java
+++ stats/stats-service/src/main/java/ru/practicum/stats/exceptions/ExceptionsHandler.java
@@ -3,14 +3,15 @@ package ru.practicum.stats.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionsHandler {
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<Object> handleValidation(ValidationException exception) {
+    @ExceptionHandler({ValidationException.class, MissingServletRequestParameterException.class})
+    public ResponseEntity<Object> handleValidation(Exception exception) {
         log.error(exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
